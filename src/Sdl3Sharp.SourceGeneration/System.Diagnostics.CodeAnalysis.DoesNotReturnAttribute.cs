548902f8a8eaeebacd794080cb5c47b3ae9680a1
Felix Rüdiger <fruediger.90@googlemail.com>
2025-09-29 12:54:55 +0200

    Repository restructure - part 2

--- src/Sdl3Sharp.SourceGeneration/System.Diagnostics.CodeAnalysis.DoesNotReturnAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿#pragma warning disable IDE0130
-namespace System.Diagnostics.CodeAnalysis;
-#pragma warning restore IDE0130
-
-[AttributeUsage(AttributeTargets.Method, Inherited = false)]
-internal sealed class DoesNotReturnAttribute : Attribute;
--- src/Sdl3Sharp.SourceGeneration/System.Diagnostics.CodeAnalysis.NotNullWhenAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿#pragma warning disable IDE0130
-namespace System.Diagnostics.CodeAnalysis;
-#pragma warning restore IDE0130
-
-[AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-internal sealed class NotNullWhenAttribute(bool returnValue) : Attribute
-{
-	public bool ReturnValue => returnValue;
-}
