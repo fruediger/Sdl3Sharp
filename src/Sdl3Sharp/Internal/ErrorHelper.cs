efcfc74e24358b09791618601b539f832c71de79
Felix Rüdiger <fruediger.90@googlemail.com>
2026-03-05 16:48:48 +0100

    Added unconditional overloads to ErrorHelper

--- src/Sdl3Sharp/Internal/ErrorHelper.cs
+++ src/Sdl3Sharp/Internal/ErrorHelper.cs
@@ -8,21 +8,17 @@ namespace Sdl3Sharp.Internal;
 
 internal static class ErrorHelper
 {
-#pragma warning disable IDE0051
 	internal const string ParameterInvalidErrorFormat = "Parameter '{0}' is invalid";
-#pragma warning restore IDE0051
 
 	/// <exception cref="SdlException">
-	/// <paramref name="condition"/> is <c><see langword="false"/></c>
-	/// and there was a <see cref="Error">SDL error</see> to throw
+	/// There was a <see cref="Error">SDL error</see> to throw
 	/// </exception>
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-	internal static void ThrowIfFailed(bool condition)
+	internal static void ThrowIfFailed()
 	{
 		unsafe
 		{
-			if (!condition
-				&& Error.SDL_GetError() is var message
+			if (Error.SDL_GetError() is var message
 				&& message is not null)
 			{
 				fail(message);
@@ -34,17 +30,27 @@ internal static class ErrorHelper
 	}
 
 	/// <exception cref="SdlException">
-	/// <paramref name="condition"/> is <c><see langword="false"/></c>,
-	/// there was a <see cref="Error">SDL error</see> to throw,
-	/// and that error did not match <paramref name="filterError"/>
+	/// <paramref name="condition"/> is <c><see langword="false"/></c>
+	/// and there was a <see cref="Error">SDL error</see> to throw
 	/// </exception>
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-	internal static void ThrowIfFailed(bool condition, ReadOnlySpan<byte> filterError)
+	internal static void ThrowIfFailed(bool condition)
+	{
+		if (!condition)
+		{
+			ThrowIfFailed();
+		}
+	}
+
+	/// <exception cref="SdlException">
+	/// There was a <see cref="Error">SDL error</see> to throw
+	/// </exception>
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	internal static void ThrowIfFailed(ReadOnlySpan<byte> filterError)
 	{
 		unsafe
 		{
-			if (!condition
-				&& Error.SDL_GetError() is var message
+			if (Error.SDL_GetError() is var message
 				&& message is not null
 				&& !MemoryMarshal.CreateReadOnlySpanFromNullTerminated(message).SequenceEqual(filterError) /* filter out errors */)
 			{
@@ -53,6 +59,21 @@ internal static class ErrorHelper
 
 			[DoesNotReturn]
 			static void fail(byte* message) => throw new SdlException(Utf8StringMarshaller.ConvertToManaged(message));
+		}
+	}
+
+
+	/// <exception cref="SdlException">
+	/// <paramref name="condition"/> is <c><see langword="false"/></c>,
+	/// there was a <see cref="Error">SDL error</see> to throw,
+	/// and that error did not match <paramref name="filterError"/>
+	/// </exception>
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	internal static void ThrowIfFailed(bool condition, ReadOnlySpan<byte> filterError)
+	{
+		if (!condition)
+		{
+			ThrowIfFailed(filterError);
 		}
 	}
 }
